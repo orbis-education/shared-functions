e04e6da098727897a38300f644a76a70ecb2389a
mrfisher-oe <michael.fisher@orbiseducation.com>
2022-05-08 21:03:00 -0400

    updated sortObjectArrayByProperty

--- index.js
+++ index.js
@@ -989,7 +989,7 @@ export const sortObjectArrayByProperty = (objectArray, sortProperty, direction) 
 
     } else {
 
-      sortedArray.sort((a, b) => (a[sortProperty] > b[sortProperty]) ? 1 : -1);
+      sortedArray.sort((a, b) => (a[sortProperty] > b[sortProperty]) ? -1 : 1);
 
       // sortedArray.sort((a, b) => (formatLowerCase(a[sortProperty]).replace("the ", "").replace("a ", "") > formatLowerCase(b[sortProperty]).replace("the ", "").replace("a ", "")) ? 1 : -1);
 
